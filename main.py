ebce37180b0a89aa2e69cc41c43777919b15235c
SimonMably <simonmably@gmail.com>
2021-11-04 20:07:17 +0000

    Updated requirements.txt, changed database from SQLite to Postgres, added Procfile, changed environment variables, site deployed on Heroku.

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ import os
 from forms import CreateAdminForm, AdminLoginForm, AddProjectForm
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "SECRET_KEY"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 ckeditor = CKEditor(app)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///portfolio.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///portfolio.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
