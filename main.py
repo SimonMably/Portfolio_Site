dc9ec14ba5ac1e5cfbee8830d3b8c972cf976907
SimonMably <simonmably@gmail.com>
2021-11-05 20:54:53 +0000

    Attempting to fix issues, possibly with database /5

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ ckeditor = CKEditor(app)
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///portfolio.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL", "sqlite:///portfolio.db")
 db = SQLAlchemy(app)
 
 
