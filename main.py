dba5044153dc3eee6519a849832ef76af82251d0
SimonMably <simonmably@gmail.com>
2021-12-19 20:18:22 +0000

    Improved mobile responsiveness.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 Bootstrap(app)
 ckeditor = CKEditor(app)
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL", "sqlite:///portfolio.db")
 db = SQLAlchemy(app)
