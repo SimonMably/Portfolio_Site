4c0e3f80ecaf57f7083ebc8d9a4b46048501bad8
SimonMably <simonmably@gmail.com>
2022-02-25 13:26:54 +0000

    Removed unused imports, Bootstrap initialisations and files. Updated requirements.txt.

--- portfolio_site/__init__.py
+++ portfolio_site/__init__.py
@@ -2,18 +2,14 @@ from datetime import datetime as dt
 import os
 
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from sqlalchemy.orm import query
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor
 from flask_login import UserMixin, login_user, LoginManager, login_required, \
     current_user, logout_user
 
-IMG_UPLOAD_FOLDER = "static/images/project-imgs"
-ALLOWED_IMG_EXTENSIONS = {"jpg", "png"}
 
 app = Flask(__name__)
-Bootstrap(app)
 ckeditor = CKEditor(app)
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "SECRET_KEY")
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from portfolio_site import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
